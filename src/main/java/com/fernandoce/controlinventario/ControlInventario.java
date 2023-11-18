ebded4f1916f054c77cf5e6b1f88e76556184035
Luis Fernando Cornejo Effio <lfernandoce96@gmail.com>
2023-11-18 18:19:54 -0500

    Crear Proyecto

---
+++ src/main/java/com/fernandoce/controlinventario/ControlInventario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fernandoce.controlinventario;
+
+/**
+ *
+ * @author lfern
+ */
+public class ControlInventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
